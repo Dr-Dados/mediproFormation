7ea1e481f0855429b43a962773894b3d9bb8e2e5
Dr.Dados <adnane.bachchar@gmail.com>
2024-04-22 09:17:35 +0100

    improve courses display

--- src/features/courses/Courses.jsx
+++ src/features/courses/Courses.jsx
@@ -2,18 +2,37 @@ import { Link } from "react-router-dom";
 import CourseItem from "./CourseItem";
 
 import { useCourses } from "../../context/CourseProvider";
+import {
+  Card,
+  CardBody,
+  List,
+  ListItem,
+  Typography,
+} from "@material-tailwind/react";
 
 function Courses() {
   const { courses } = useCourses();
   return (
     <div>
-      <h2 className="text-xl font-semibold mb-5">Formations</h2>
-      <div className="grid grid-cols-3 gap-2">
-        {courses.map((course) => (
-          <Link key={course.id} to={`${course.id}`}>
-            <CourseItem color={course.color}>{course.title}</CourseItem>
-          </Link>
-        ))}
+      <div>
+        <h2 className="text-3xl font-semibold my-4">Formations</h2>
+        <Card>
+          <CardBody>
+            <List className="p-0">
+              {courses.map((course) => (
+                <Link key={course.id} to={`${course.id}`}>
+                  <ListItem key={course.id}>
+                    <Typography>
+                      <h5 className="font-normal  text-gray-900">
+                        {course.title}
+                      </h5>
+                    </Typography>
+                  </ListItem>
+                </Link>
+              ))}
+            </List>
+          </CardBody>
+        </Card>
       </div>
     </div>
   );
